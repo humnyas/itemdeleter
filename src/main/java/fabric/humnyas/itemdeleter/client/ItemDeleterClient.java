9acbc623e7bef254d63f2d00b81cba317a63bc59
humnyas <180968941+humnyas@users.noreply.github.com>
2025-05-26 13:27:34 +0100

    Initial commit

---
+++ src/main/java/fabric/humnyas/itemdeleter/client/ItemDeleterClient.java
@@ -0,0 +1,9 @@
+package fabric.humnyas.itemdeleter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemDeleterClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {
+
+    }
+}
